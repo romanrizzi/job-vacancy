6d7ede255980f6019373ba163a56e9fc22aa154c
patriciotella <patriciotella@gmail.com>
2015-06-04 23:01:59 -0300

    add second step when i visit de offer page

--- features/step_definitions/visit_counter_for_job_offers_steps.rb
+++ features/step_definitions/visit_counter_for_job_offers_steps.rb
@@ -8,7 +8,11 @@ Given(/^I visit an offer twice$/) do
 end
 
 When(/^I visit de offer page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit '/login'
+  fill_in('user[email]', :with => 'offerer@test.com')
+  fill_in('user[password]', :with => 'Passw0rd!')
+  click_button('Login')
+  visit '/job_offers/my'
 end
 
 Then(/^I should see that the offer's been visited (\d+) times$/) do |number_of_visits|
