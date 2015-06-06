b426b684046c1bd27f74c903eeb56a9c12af3eb5
Roman <rizziromanalejandro@gmail.com>
2015-06-05 22:55:24 -0300

    Adding third Scenario: Given two offers, only the non expired one is visible

--- features/offers_with_expiration_date.feature
+++ features/offers_with_expiration_date.feature
@@ -4,3 +4,9 @@ Feature: Offers have an expiration date.
     Given an expired offer
     When I access the offers list page
     Then this offer should not appear in this page
+
+  Scenario: Only non expired offer is shown
+    Given a non expired offer
+    And a expired offer
+    When I access the offers list page
+    Then The non expired one should be the only visible in this page
--- features/step_definitions/offers_with_expiration_date_steps.rb
+++ features/step_definitions/offers_with_expiration_date_steps.rb
@@ -1,11 +1,28 @@
 
 Given(/^an expired offer$/) do
-  visit '/job_offers/new'
-  fill_in('job_offer[title]', :with => "Arquitecto Java")
-  fill_in('job_offer[expiration_date]', :with => (Date.today -1).to_s)
-  click_button('Create')
+  create_expired_offer
 end
 
 Then(/^this offer should not appear in this page$/) do
   page.should_not have_content("Arquitecto Java")
+end
+
+Given(/^a non expired offer$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a expired offer$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The non expired one should be the only visible in this page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+
+def create_expired_offer
+  visit '/job_offers/new'
+  fill_in('job_offer[title]', :with => "Arquitecto Java")
+  fill_in('job_offer[expiration_date]', :with => (Date.today -1).to_s)
+  click_button('Create')
 end
