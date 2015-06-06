8d6cbda96d33f9946dfa78d5145526abf79a0be2
Roman <rizziromanalejandro@gmail.com>
2015-06-05 22:51:07 -0300

    Adding second Scenario

--- features/offers_with_expiration_date.feature
+++ features/offers_with_expiration_date.feature
@@ -1,6 +1,5 @@
 Feature: Offers have an expiration date.
 
-  @wip
   Scenario: Expired offer is not shown
     Given an expired offer
     When I access the offers list page
--- features/step_definitions/offers_with_expiration_date_steps.rb
+++ features/step_definitions/offers_with_expiration_date_steps.rb
@@ -1,8 +1,11 @@
 
 Given(/^an expired offer$/) do
-  pending # express the regexp above with the code you wish you had
+  visit '/job_offers/new'
+  fill_in('job_offer[title]', :with => "Arquitecto Java")
+  fill_in('job_offer[expiration_date]', :with => (Date.today -1).to_s)
+  click_button('Create')
 end
 
 Then(/^this offer should not appear in this page$/) do
-  pending # express the regexp above with the code you wish you had
+  page.should_not have_content("Arquitecto Java")
 end
