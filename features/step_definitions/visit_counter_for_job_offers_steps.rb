29b5f5b6ea6c265bb143664d58d9bb4f0eb7842b
patriciotella <patriciotella@gmail.com>
2015-06-06 01:34:08 -0300

    refactor: delete background from feature, creat new methods in the step definitions to avoid repeating code

--- features/step_definitions/visit_counter_for_job_offers_steps.rb
+++ features/step_definitions/visit_counter_for_job_offers_steps.rb
@@ -1,4 +1,5 @@
 Given(/^I visit an offer twice$/) do
+  create_new_offer_with 'First Offer','Bernal','The one'
   visit '/job_offers'
   click_link 'Apply'
   fill_in('job_application[applicant_email]', :with => 'applicant1@test.com')
@@ -17,24 +18,28 @@ When(/^I visit the offer page$/) do
 end
 
 Then(/^I should see that the offer's been visited (\d+) times$/) do |number_of_visits|
-  assert_there_is_a_number_of_visits_in_the_correct_row number_of_visits.to_i,2, 5
+  assert_there_is_a_number_of_visits_in_the_correct_row_and_column number_of_visits.to_i,2, 5
 end
 
 Given(/^I create a new offer$/) do
-  @job_offer = JobOffer.new
-  @job_offer.owner = User.first
-  @job_offer.title = 'new job offer'
-  @job_offer.location = 'Wilde'
-  @job_offer.description = 'Cool job'
-  @job_offer.save
+  create_new_offer_with 'new job offer','Wilde','Cool job'
 end
 
 Then(/^I should see that the offer has (\d+) visits$/) do |number_of_visits|
-  assert_there_is_a_number_of_visits_in_the_correct_row number_of_visits.to_i,3,5
+  assert_there_is_a_number_of_visits_in_the_correct_row_and_column number_of_visits.to_i,3,5
 end
 
 
-def assert_there_is_a_number_of_visits_in_the_correct_row number_of_visits,a_row, a_column
+def create_new_offer_with a_name,a_location,a_description
+  @job_offer = JobOffer.new
+  @job_offer.owner = User.first
+  @job_offer.title = a_name
+  @job_offer.location = a_location
+  @job_offer.description = a_description
+  @job_offer.save
+end
+
+def assert_there_is_a_number_of_visits_in_the_correct_row_and_column number_of_visits,a_row, a_column
   visit '/job_offers/my'
   within("table tr:nth-child(#{a_row})") do
   expect(find("td:nth-child(#{a_column})").text.to_i).to eq number_of_visits
--- features/visit_counter_for_job_offers.feature
+++ features/visit_counter_for_job_offers.feature
@@ -1,14 +1,10 @@
 Feature: Counting the amount of visits in a job application
 
-  Background:
-    Given only a "Web Programmer" offer exists in the offers list
-
   Scenario:  Offer visited twice
     Given I visit an offer twice
     When I visit the offer page
     Then I should see that the offer's been visited 2 times
 
-  #@wip
   Scenario: New offer has no visits
     Given I create a new offer
     When I visit the offer page
