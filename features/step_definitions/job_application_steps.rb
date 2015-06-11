4d0003a24a1e2b0082ee26e29c2b39e908c02637
Roman <rizziromanalejandro@gmail.com>
2015-06-10 23:39:34 -0300

    Fixing tmp mails error

--- features/step_definitions/job_application_steps.rb
+++ features/step_definitions/job_application_steps.rb
@@ -11,15 +11,10 @@ Given(/^I access the offers list page$/) do
   visit '/job_offers'
 end
 
-When(/^I apply$/) do
-  click_link 'Apply'
-  fill_in('job_application[applicant_email]', :with => 'applicant@test.com')
-  click_button('Apply')
-end
 
 Then(/^I should receive a mail with offerer info$/) do
   mail_store = "#{Padrino.root}/tmp/emails"
-  file = File.open("#{mail_store}/applicant@test.com", "r")
+  file = File.open("#{mail_store}/pepe@example.com", "r")
   content = file.read
   content.include?(@job_offer.title).should be true
   content.include?(@job_offer.location).should be true
