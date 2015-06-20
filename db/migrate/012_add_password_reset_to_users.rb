3c21b6ccc185e8adcd62f8c6f9dc2dd7162dd78b
jcalvento <julian.calvento@10pines.com>
2015-06-20 19:26:27 -0300

    Fix migration down

--- db/migrate/012_add_password_reset_to_users.rb
+++ db/migrate/012_add_password_reset_to_users.rb
@@ -7,7 +7,7 @@ migration 12, :add_password_reset_to_users do
   end
 
   down do
-    modify_table :job_offers do
+    modify_table :users do
       drop_column :password_reset_token
       drop_column :password_reset_generated_at
     end
