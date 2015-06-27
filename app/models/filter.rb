1035a576b9ae0cc226d5cac35e2561b79675825c
jcalvento <julian.calvento@10pines.com>
2015-06-27 17:48:50 -0300

    Test: searching offers by multiple fields

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,18 +1,18 @@
-class Filter
+class JobVacancy::Filter
 
   def initialize a_class
     @object_to_filter = a_class
   end
 
   def call a_query
-    split_query = a_query.split('')
-    field = a_query[0 .. a_query.index(split_query.detect { |char| char == ':' }) - 1]
-    index = a_query.index(field)
-    if index.present?
-      if a_query[index + field.size] == ':'
-        value = a_query[index + field.size + 1 ... a_query.size]
-        @object_to_filter.all(field.to_sym.like => "%#{value}%")
+    a_query.split(' & ').inject([]) { |result, query|
+      split_query = query.split('')
+      field = query[0 .. query.index(split_query.detect { |char| char == ':' }) - 1]
+      index = query.index(field)
+      if query[index + field.size] == ':'
+        value = query[index + field.size + 1 ... query.size]
+        result.concat @object_to_filter.all(field.to_sym.like => "%#{value}%")
       end
-    end
+    }
   end
 end
--- spec/app/models/filter_spec.rb
+++ spec/app/models/filter_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
-describe Filter do
+describe JobVacancy::Filter do
 
   let(:user) { User.create(name: 'Test User', password: '123abc', email: 'test@user.com') }
   let(:java_dev_offer) { JobOffer.create(title: 'Java Developer', description: 'A good choice', user: user) }
   let(:offer) { JobOffer.create(title: 'Another developer offer', description: 'Another good choice',  user: user) }
-  let(:filter) { Filter.new(JobOffer) }
+  let(:filter) { JobVacancy::Filter.new(JobOffer) }
+
+  before :each do
+    JobOffer.all.destroy
+    java_dev_offer
+    offer
+  end
 
   it 'should filter offers by their titles' do
     offers = filter.call('title:java')
@@ -17,5 +23,11 @@ describe Filter do
     offers = filter.call('description:another')
 
     expect(offers).to contain_exactly offer
+  end
+
+  it 'should filter offers by the selected fields' do
+    offers = filter.call('title:java & description:another')
+
+    expect(offers).to eq [java_dev_offer, offer]
   end
 end
