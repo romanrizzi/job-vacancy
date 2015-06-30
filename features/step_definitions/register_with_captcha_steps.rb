0d5289ace1bacd36713d34ec86a70e5f4b948439
patriciotella <patriciotella@gmail.com>
2015-06-30 19:52:28 -0300

    Migrating captcha to register page Feature refactor (wip tagged)

--- features/step_definitions/register_with_captcha_steps.rb
+++ features/step_definitions/register_with_captcha_steps.rb
@@ -1,8 +1,10 @@
-Given(/^Im on the login page and I fill in email with "(.*?)" and password with "(.*?)"$/) do |email, password|
+Given(/^Im on the Register page and I fill in name with "(.*?)" email with "(.*?)" and password with "(.*?)"$/) do |name, email, password|
 
-  visit '/login'
+  visit '/register'
+  fill_in('user[name]', :with => name)
   fill_in('user[email]', :with => email)
   fill_in('user[password]', :with => password)
+  fill_in('user[password_confirmation]', :with => password)
 
 end
 
@@ -11,15 +13,14 @@ Given(/^I fill in the captcha textbox with the correct image$/) do
   Rack::Recaptcha.test_mode! #set the captcha response, its give me true
 end
 
-When(/^I click login$/) do
+When(/^I click Create$/) do
 
-  click_button('Login')
+  click_button('Create')
 
 end
 
-Then(/^I should be logged in$/) do
-  page.should have_content('offerer@test.com')
-  page.should have_content('Logout')
+Then(/^I should see the message "(.*?)"$/) do |message|
+  page.should have_content(message)
 end
 
 Given(/^I fill in the captcha textbox with invalid data$/) do
